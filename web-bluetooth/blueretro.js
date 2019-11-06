783efa4a0ff95b50a3f8116dbe4949384d1aeddf
Jacques Gagnon <darthcloud@gmail.com>
2019-11-06 07:36:33 -0500

    Fixup 128bit UUID

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -3,18 +3,18 @@ let configChar = null;
 function onButtonClick() {
   log('Requesting Bluetooth Device...');
   navigator.bluetooth.requestDevice(
-    {filters: [{services: ['569a7976a12f4b31b0fa8051560f8300']}]})
+    {filters: [{services: ['569a7976-a12f-4b31-b0fa-8051560f8300']}]})
   .then(device => {
     log('Connecting to GATT Server...');
     return device.gatt.connect();
   })
   .then(server => {
     log('Getting BlueRetro Service...');
-    return server.getPrimaryService('569a7976a12f4b31b0fa8051560f8300');
+    return server.getPrimaryService('569a7976-a12f-4b31-b0fa-8051560f8300');
   })
   .then(service => {
     log('Getting Config Characteristic...');
-    return  service.getCharacteristic('569a7976a12f4b31b0fa8051560f8301');
+    return  service.getCharacteristic('569a7976-a12f-4b31-b0fa-8051560f8301');
   })
   .then(characteristic => {
     log('Reading Config...');
