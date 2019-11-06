beb8a2c2dead13625acae46b6b9ba60589bfb75d
Jacques Gagnon <darthcloud@gmail.com>
2019-11-06 08:24:49 -0500

    UUID little endian?

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -4,14 +4,14 @@ function onButtonClick() {
   log('Requesting Bluetooth Device...');
   navigator.bluetooth.requestDevice(
     {filters: [{name: 'BlueRetro'}]},
-    {optionalServices: ['569a7976-a12f-4b31-b0fa-8051560f8300']})
+    {optionalServices: ['00830f56-5180-fab0-314b-2fa176799a56']})
   .then(device => {
     log('Connecting to GATT Server...');
     return device.gatt.connect();
   })
   .then(server => {
     log('Getting BlueRetro Service...');
-    return server.getPrimaryService('569a7976-a12f-4b31-b0fa-8051560f8300');
+    return server.getPrimaryService('00830f56-5180-fab0-314b-2fa176799a56');
   })
   .then(service => {
     log('Getting Config Characteristic...');
