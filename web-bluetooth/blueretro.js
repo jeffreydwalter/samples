862b701299cead1909e400d4f5133913599332b5
Jacques Gagnon <darthcloud@gmail.com>
2019-11-08 11:15:15 -0500

    test jquery field

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -58,3 +58,18 @@ function onButtonClick2() {
     log('Argh! ' + error);
   });
 }
+
+function onButtonClick3() {
+    var max_fields_limit      = 8; //set limit for maximum input fields
+    var x = 1; //initialize counter for text box
+    $('.add_more_button').click(function(e){ //click event on add more fields button having class add_more_button
+        e.preventDefault();
+        if(x < max_fields_limit){ //check conditions
+            x++; //counter increment
+            $('.input_fields_container_part').append('<div><input type="text" name="tags"/><a href="#" class="remove_field" style="margin-left:10px;">Remove</a></div>'); //add input field
+        }
+    });
+    $('.input_fields_container_part').on("click",".remove_field", function(e){ //user click on remove text links
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+}
