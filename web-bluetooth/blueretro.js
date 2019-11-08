82feb973ccdafd5ac0a23a6c6e6a6991ad077b47
Jacques Gagnon <darthcloud@gmail.com>
2019-11-08 07:17:43 -0500

    Add UUID array

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -1,21 +1,38 @@
+var brUuid = [
+    '56830f56-5180-fab0-314b-2fa176799a00',
+    '56830f56-5180-fab0-314b-2fa176799a01',
+    '56830f56-5180-fab0-314b-2fa176799a02',
+    '56830f56-5180-fab0-314b-2fa176799a03',
+    '56830f56-5180-fab0-314b-2fa176799a04',
+    '56830f56-5180-fab0-314b-2fa176799a05',
+    '56830f56-5180-fab0-314b-2fa176799a06',
+    '56830f56-5180-fab0-314b-2fa176799a07',
+    '56830f56-5180-fab0-314b-2fa176799a08',
+    '56830f56-5180-fab0-314b-2fa176799a09',
+    '56830f56-5180-fab0-314b-2fa176799a0a',
+    '56830f56-5180-fab0-314b-2fa176799a0b',
+    '56830f56-5180-fab0-314b-2fa176799a0c',
+    '56830f56-5180-fab0-314b-2fa176799a0d',
+];
+
 let configChar = null;
 
 function onButtonClick() {
   log('Requesting Bluetooth Device...');
   navigator.bluetooth.requestDevice(
     {filters: [{name: 'BlueRetro'}],
-    optionalServices: ['00830f56-5180-fab0-314b-2fa176799a56']})
+    optionalServices: [brUuid[0]]})
   .then(device => {
     log('Connecting to GATT Server...');
     return device.gatt.connect();
   })
   .then(server => {
     log('Getting BlueRetro Service...');
-    return server.getPrimaryService('00830f56-5180-fab0-314b-2fa176799a56');
+    return server.getPrimaryService(brUuid[0]);
   })
   .then(service => {
     log('Getting Config Characteristic...');
-    return  service.getCharacteristic('01830f56-5180-fab0-314b-2fa176799a56');
+    return  service.getCharacteristic(brUuid[1]);
   })
   .then(characteristic => {
     log('Reading Config...');
