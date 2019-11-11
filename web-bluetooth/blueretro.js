f84d0be26eba2730b630ed686b4efeba3521d16f
Jacques Gagnon <darthcloud@gmail.com>
2019-11-11 07:05:30 -0500

    fixup

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -233,7 +233,7 @@ function initField() {
     divCfg.appendChild(dz);
 
     /* Turbo */
-    var turbo = document.createElement("select");
+/*    var turbo = document.createElement("select");
     turbo.setAttribute("style", "max-width:10%;");
     for (var i = 0; i < maxTurbo; i++) {
         var option  = document.createElement("option");
@@ -248,9 +248,9 @@ function initField() {
     }
     turbo.setAttribute("class", "turbo");
     divCfg.appendChild(turbo);
-
+*/
     /* Scaling */
-    var sca = document.createElement("select");
+/*    var sca = document.createElement("select");
     sca.setAttribute("style", "max-width:10%;");
     for (var i = 0; i < scaling.length; i++) {
         var option  = document.createElement("option");
@@ -260,7 +260,7 @@ function initField() {
     }
     sca.setAttribute("class", "scaling");
     divCfg.appendChild(sca);
-
+*/
     /* Scaling diag */
     var diag = document.createElement("select");
     diag.setAttribute("style", "max-width:10%;");
