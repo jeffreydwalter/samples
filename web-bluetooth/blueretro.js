d7be9dd955cd6eac3b38a054c783957a75a0ac2e
Jacques Gagnon <darthcloud@gmail.com>
2019-11-11 15:04:42 -0500

    fixup

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -439,9 +439,8 @@ function loadGlobalCfg() {
     })
     .then(value => {
         log('Global Config size: ' + value.byteLength);
-        log('First byte: ' + value.getUint8(0));
-        document.getElementById("systemCfg").value(value.getUint16(3));
-        document.getElementById("multitapCfg").value(value.getUint16(4));
+        document.getElementById("systemCfg").value(value.getUint16(2));
+        document.getElementById("multitapCfg").value(value.getUint16(3));
     })
     .catch(error => {
         log('Argh! ' + error);
