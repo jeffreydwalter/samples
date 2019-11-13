2b70519f074e4b65d13dbd49a75e8d4e1126005d
Jacques Gagnon <darthcloud@gmail.com>
2019-11-13 10:56:40 -0500

    fixup

--- web-bluetooth/blueretro.js
+++ web-bluetooth/blueretro.js
@@ -473,7 +473,8 @@ function writeReadRecursive(cfg, inputCtrl, ctrl_chrc, data_chrc) {
         })
         .then(value => {
             log('Got Input Data ' + value.byteLength);
-            cfg.set(Int8Array(value.buffer), cfg.lenght);
+            var tmp = new Uint8Array(value.buffer);
+            cfg.set(tmp, cfg.lenght);
             log('Got Input Data ' + cfg[2] + ' ' + value.getUint8(2));
             if (value.byteLength == 512) {
                 inputCtrl[1] += Number(512);
