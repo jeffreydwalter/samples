18bc525333fc87b2d2de055e745907cd4fa68c09
Jacques Gagnon <darthcloud@gmail.com>
2020-01-02 10:06:29 -0500

    Use data from json files

--- web-bluetooth/blueretro_presets.js
+++ web-bluetooth/blueretro_presets.js
@@ -158,217 +158,6 @@ const btn = {
     "KB_RWIN":101,
 };
 
-var presetsName = [
-    'Default PAD+KB+M',
-    'Gamepad only',
-    'N64 GoldenEye 007 / Perfect Dark 1.2',
-    'N64 GoldenEye 007 / Perfect Dark 2.2 SP',
-    'N64 GoldenEye 007 / Perfect Dark 2.2 MP',
-];
-
-var presets = [
-    [   /* Default PAD+KB+M */
-        [0, 0, 0, 100, 50, 135, 0, 0, 0],
-        [1, 1, 0, 100, 50, 135, 0, 0, 0],
-        [2, 2, 0, 100, 50, 135, 0, 0, 0],
-        [3, 3, 0, 100, 50, 135, 0, 0, 0],
-        [4, 4, 0, 100, 50, 135, 0, 0, 0],
-        [5, 5, 0, 100, 50, 135, 0, 0, 0],
-        [6, 6, 0, 100, 50, 135, 0, 0, 0],
-        [7, 7, 0, 100, 50, 135, 0, 0, 0],
-        [8, 8, 0, 100, 50, 135, 0, 0, 0],
-        [9, 9, 0, 100, 50, 135, 0, 0, 0],
-        [10, 10, 0, 100, 50, 135, 0, 0, 0],
-        [11, 11, 0, 100, 50, 135, 0, 0, 0],
-        [12, 12, 0, 100, 50, 135, 0, 0, 0],
-        [13, 13, 0, 100, 50, 135, 0, 0, 0],
-        [14, 14, 0, 100, 50, 135, 0, 0, 0],
-        [15, 15, 0, 100, 50, 135, 0, 0, 0],
-        [16, 16, 0, 100, 50, 135, 0, 0, 0],
-        [17, 17, 0, 100, 50, 135, 0, 0, 0],
-        [18, 18, 0, 100, 50, 135, 0, 0, 0],
-        [19, 19, 0, 100, 50, 135, 0, 0, 0],
-        [20, 20, 0, 100, 50, 135, 0, 0, 0],
-        [21, 21, 0, 100, 50, 135, 0, 0, 0],
-        [22, 22, 0, 100, 50, 135, 0, 0, 0],
-        [23, 23, 0, 100, 50, 135, 0, 0, 0],
-        [24, 24, 0, 100, 50, 135, 0, 0, 0],
-        [25, 25, 0, 100, 50, 135, 0, 0, 0],
-        [26, 26, 0, 100, 50, 135, 0, 0, 0],
-        [27, 27, 0, 100, 50, 135, 0, 0, 0],
-        [28, 28, 0, 100, 50, 135, 0, 0, 0],
-        [29, 29, 0, 100, 50, 135, 0, 0, 0],
-        [30, 30, 0, 100, 50, 135, 0, 0, 0],
-        [31, 31, 0, 100, 50, 135, 0, 0, 0],
-        [32, 32, 0, 100, 50, 135, 0, 0, 0],
-        [33, 33, 0, 100, 50, 135, 0, 0, 0],
-        [34, 34, 0, 100, 50, 135, 0, 0, 0],
-        [35, 35, 0, 100, 50, 135, 0, 0, 0],
-        [36, 36, 0, 100, 50, 135, 0, 0, 0],
-        [37, 37, 0, 100, 50, 135, 0, 0, 0],
-        [38, 38, 0, 100, 50, 135, 0, 0, 0],
-        [39, 39, 0, 100, 50, 135, 0, 0, 0],
-        [40, 40, 0, 100, 50, 135, 0, 0, 0],
-        [41, 41, 0, 100, 50, 135, 0, 0, 0],
-        [42, 42, 0, 100, 50, 135, 0, 0, 0],
-        [43, 43, 0, 100, 50, 135, 0, 0, 0],
-        [44, 44, 0, 100, 50, 135, 0, 0, 0],
-        [45, 45, 0, 100, 50, 135, 0, 0, 0],
-        [46, 46, 0, 100, 50, 135, 0, 0, 0],
-        [47, 47, 0, 100, 50, 135, 0, 0, 0],
-        [48, 48, 0, 100, 50, 135, 0, 0, 0],
-        [49, 49, 0, 100, 50, 135, 0, 0, 0],
-        [50, 50, 0, 100, 50, 135, 0, 0, 0],
-        [51, 51, 0, 100, 50, 135, 0, 0, 0],
-        [52, 52, 0, 100, 50, 135, 0, 0, 0],
-        [53, 53, 0, 100, 50, 135, 0, 0, 0],
-        [54, 54, 0, 100, 50, 135, 0, 0, 0],
-        [55, 55, 0, 100, 50, 135, 0, 0, 0],
-        [56, 56, 0, 100, 50, 135, 0, 0, 0],
-        [57, 57, 0, 100, 50, 135, 0, 0, 0],
-        [58, 58, 0, 100, 50, 135, 0, 0, 0],
-        [59, 59, 0, 100, 50, 135, 0, 0, 0],
-        [60, 60, 0, 100, 50, 135, 0, 0, 0],
-        [61, 61, 0, 100, 50, 135, 0, 0, 0],
-        [62, 62, 0, 100, 50, 135, 0, 0, 0],
-        [63, 63, 0, 100, 50, 135, 0, 0, 0],
-        [64, 64, 0, 100, 50, 135, 0, 0, 0],
-        [65, 65, 0, 100, 50, 135, 0, 0, 0],
-        [66, 66, 0, 100, 50, 135, 0, 0, 0],
-        [67, 67, 0, 100, 50, 135, 0, 0, 0],
-        [68, 68, 0, 100, 50, 135, 0, 0, 0],
-        [69, 69, 0, 100, 50, 135, 0, 0, 0],
-        [70, 70, 0, 100, 50, 135, 0, 0, 0],
-        [71, 71, 0, 100, 50, 135, 0, 0, 0],
-        [72, 72, 0, 100, 50, 135, 0, 0, 0],
-        [73, 73, 0, 100, 50, 135, 0, 0, 0],
-        [74, 74, 0, 100, 50, 135, 0, 0, 0],
-        [75, 75, 0, 100, 50, 135, 0, 0, 0],
-        [76, 76, 0, 100, 50, 135, 0, 0, 0],
-        [77, 77, 0, 100, 50, 135, 0, 0, 0],
-        [78, 78, 0, 100, 50, 135, 0, 0, 0],
-        [79, 79, 0, 100, 50, 135, 0, 0, 0],
-        [80, 80, 0, 100, 50, 135, 0, 0, 0],
-        [81, 81, 0, 100, 50, 135, 0, 0, 0],
-        [82, 82, 0, 100, 50, 135, 0, 0, 0],
-        [83, 83, 0, 100, 50, 135, 0, 0, 0],
-        [84, 84, 0, 100, 50, 135, 0, 0, 0],
-        [85, 85, 0, 100, 50, 135, 0, 0, 0],
-        [86, 86, 0, 100, 50, 135, 0, 0, 0],
-        [87, 87, 0, 100, 50, 135, 0, 0, 0],
-        [88, 88, 0, 100, 50, 135, 0, 0, 0],
-        [89, 89, 0, 100, 50, 135, 0, 0, 0],
-        [90, 90, 0, 100, 50, 135, 0, 0, 0],
-        [91, 91, 0, 100, 50, 135, 0, 0, 0],
-        [92, 92, 0, 100, 50, 135, 0, 0, 0],
-        [93, 93, 0, 100, 50, 135, 0, 0, 0],
-        [94, 94, 0, 100, 50, 135, 0, 0, 0],
-        [95, 95, 0, 100, 50, 135, 0, 0, 0],
-        [96, 96, 0, 100, 50, 135, 0, 0, 0],
-        [97, 97, 0, 100, 50, 135, 0, 0, 0],
-        [98, 98, 0, 100, 50, 135, 0, 0, 0],
-        [100, 100, 0, 100, 50, 135, 0, 0, 0],
-        [101, 101, 0, 100, 50, 135, 0, 0, 0],
-    ],
-    [   /* Gamepad only */
-        [btn.PAD_LX_LEFT,  btn.PAD_LX_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LX_RIGHT, btn.PAD_LX_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_DOWN,  btn.PAD_LY_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_UP,    btn.PAD_LY_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_LEFT,  btn.PAD_RX_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_RIGHT, btn.PAD_RX_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_DOWN,  btn.PAD_RY_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_UP,    btn.PAD_RY_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_LEFT,  btn.PAD_LD_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_RIGHT, btn.PAD_LD_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_DOWN,  btn.PAD_LD_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_UP,    btn.PAD_LD_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RD_LEFT,  btn.PAD_RD_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RD_RIGHT, btn.PAD_RD_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RD_DOWN,  btn.PAD_RD_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RD_UP,    btn.PAD_RD_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_LEFT,  btn.PAD_RB_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_RIGHT, btn.PAD_RB_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_DOWN,  btn.PAD_RB_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_UP,    btn.PAD_RB_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MM,       btn.PAD_MM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MS,       btn.PAD_MS,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MT,       btn.PAD_MT,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MQ,       btn.PAD_MQ,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LM,       btn.PAD_LM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LS,       btn.PAD_LS,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LT,       btn.PAD_LT,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LJ,       btn.PAD_LJ,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RM,       btn.PAD_RM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RS,       btn.PAD_RS,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RT,       btn.PAD_RT,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RJ,       btn.PAD_RJ,       0, 100, 50, 135, 0, 0, 0],
-    ],
-    [   /* N64 GoldenEye 007 / Perfect Dark 1.2 */
-        [btn.PAD_LX_LEFT,  btn.PAD_RX_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LX_RIGHT, btn.PAD_RX_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_DOWN,  btn.PAD_RY_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_UP,    btn.PAD_RY_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_LEFT,  btn.PAD_LX_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_RIGHT, btn.PAD_LX_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_DOWN,  btn.PAD_LY_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_UP,    btn.PAD_LY_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_LEFT,  btn.PAD_LD_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_RIGHT, btn.PAD_LD_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_DOWN,  btn.PAD_LD_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_UP,    btn.PAD_LD_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LM,       btn.PAD_LM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RM,       btn.PAD_RM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LS,       btn.PAD_LS,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RS,       btn.PAD_RS,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_LEFT,  btn.PAD_RB_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_DOWN,  btn.PAD_RB_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MM,       btn.PAD_MM,       0, 100, 50, 135, 0, 0, 0],
-    ],
-    [   /* N64 GoldenEye 007 / Perfect Dark 2.2 SP */
-        [btn.PAD_LX_LEFT,  btn.PAD_LX_LEFT,  1, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LX_RIGHT, btn.PAD_LX_RIGHT, 1, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_DOWN,  btn.PAD_LY_DOWN,  1, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_UP,    btn.PAD_LY_UP,    1, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_LEFT,  btn.PAD_LX_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_RIGHT, btn.PAD_LX_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_DOWN,  btn.PAD_LY_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_UP,    btn.PAD_LY_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_LEFT,  btn.PAD_LD_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_RIGHT, btn.PAD_LD_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_DOWN,  btn.PAD_LD_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_UP,    btn.PAD_LD_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LM,       btn.PAD_LM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RM,       btn.PAD_RM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LS,       btn.PAD_LM,       1, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RS,       btn.PAD_RM,       1, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_LEFT,  btn.PAD_RB_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_DOWN,  btn.PAD_RB_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MM,       btn.PAD_MM,       0, 100, 50, 135, 0, 0, 0],
-    ],
-    [   /* N64 GoldenEye 007 / Perfect Dark 2.2 MP */
-        [btn.PAD_LX_LEFT,  btn.PAD_LX_LEFT,  2, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LX_RIGHT, btn.PAD_LX_RIGHT, 2, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_DOWN,  btn.PAD_LY_DOWN,  2, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LY_UP,    btn.PAD_LY_UP,    2, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_LEFT,  btn.PAD_LX_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RX_RIGHT, btn.PAD_LX_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_DOWN,  btn.PAD_LY_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RY_UP,    btn.PAD_LY_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_LEFT,  btn.PAD_LD_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_RIGHT, btn.PAD_LD_RIGHT, 0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_DOWN,  btn.PAD_LD_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LD_UP,    btn.PAD_LD_UP,    0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LM,       btn.PAD_LM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RM,       btn.PAD_RM,       0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_LS,       btn.PAD_LM,       2, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RS,       btn.PAD_RM,       2, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_LEFT,  btn.PAD_RB_LEFT,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_RB_DOWN,  btn.PAD_RB_DOWN,  0, 100, 50, 135, 0, 0, 0],
-        [btn.PAD_MM,       btn.PAD_MM,       0, 100, 50, 135, 0, 0, 0],
-    ],
-];
-
 const maxMainInput = 12;
 const maxSubInput = 4;
 const maxOutput = 12;
@@ -376,6 +165,7 @@ const maxMax = 255;
 const maxThres = 100;
 const maxTurbo = 16;
 
+var presets = new Array();
 var bluetoothDevice;
 var maxMapping = 255;
 var nbMapping = 1;
@@ -397,10 +187,10 @@ function initInputSelect() {
     div.appendChild(main);
 
     var main = document.createElement("select");
-    for (var i = 0; i < presetsName.length; i++) {
+    for (var i = 0; i < presets.length; i++) {
         var option  = document.createElement("option");
         option.value = i;
-        option.text = presetsName[i];
+        option.text = presets[i].name;
         main.add(option);
     }
     main.id = "presetsName";
@@ -465,16 +255,11 @@ function getMapList(url) {
 }
 
 function initBlueRetroCfg() {
-    var presets = new Array();
     getMapList('https://api.github.com/repos/darthcloud/samples/contents/web-bluetooth/map/')
     .then(files => {
         return fetchMap(presets, files, 0);
     })
     .then(test => {
-        log(presets[0].name);
-        log(presets[1].name);
-        log(test[0].name);
-        log(test[1].name);
         initInputSelect();
         initOutputMapping();
     })
@@ -539,7 +324,7 @@ function writeInputCfg(cfgId, cfg) {
 
 function saveInput() {
     var preset = Number(document.getElementById("presetsName").value);
-    var nbMapping = presets[preset].length;
+    var nbMapping = presets[preset].map.length;
     var cfgSize = nbMapping*8 + 3;
     var cfg = new Uint8Array(cfgSize);
     var cfgId = Number(document.getElementById("inputSelect").value);
@@ -551,14 +336,14 @@ function saveInput() {
 
     log('Input: '+ cfgId + 'Preset: ' + preset);
     for (var i = 0; i < nbMapping; i++) {
-        cfg[j++] = presets[preset][i][0];
-        cfg[j++] = presets[preset][i][1];
-        cfg[j++] = presets[preset][i][2] + cfgId;
-        cfg[j++] = presets[preset][i][3];
-        cfg[j++] = presets[preset][i][4];
-        cfg[j++] = presets[preset][i][5];
-        cfg[j++] = presets[preset][i][6];
-        cfg[j++] = Number(presets[preset][i][7]) | (Number(presets[preset][i][8]) << 4);
+        cfg[j++] = presets[preset].map[i][0];
+        cfg[j++] = presets[preset].map[i][1];
+        cfg[j++] = presets[preset].map[i][2] + cfgId;
+        cfg[j++] = presets[preset].map[i][3];
+        cfg[j++] = presets[preset].map[i][4];
+        cfg[j++] = presets[preset].map[i][5];
+        cfg[j++] = presets[preset].map[i][6];
+        cfg[j++] = Number(presets[preset].map[i][7]) | (Number(presets[preset].map[i][8]) << 4);
     }
 
     return new Promise(function(resolve, reject) {
